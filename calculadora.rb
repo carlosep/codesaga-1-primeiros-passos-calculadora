3fc89c658e1f5cebafb5f54dd856dc4fd2b2cda6
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-11-15 10:03:55 -0200

    Primeiros passos ao caminho do Ruby - concluído.

--- calculadora.rb
+++ calculadora.rb
@@ -1,4 +1,36 @@
 class Calculadora
   def soma(n1, n2)
+	n1 + n2
+  end
+
+  def subtracao(n1, n2)
+  	n1 - n2
+  end
+
+  def multiplicacao(n1, n2)
+	n1 * n2
+  end
+
+  def divisao(n1,n2)
+	if n2 == 0
+	  0	
+	else	
+	  n1 / n2
+	end
+  end
+
+  def calcular(n1, n2, s)
+	  case "#{s}"
+      when "/"
+        n1 / n2
+      when "-"
+        n1 - n2
+      when "+"
+        n1 + n2
+      when "*"
+        n1 * n2
+      else
+        puts " get rekt"
+      end
   end
 end
